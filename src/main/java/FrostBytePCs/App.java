cb3ecd4a2f85699eeedde97095c2e2f2dfeb26a9
Sean Marriott <sma462@uclive.ac.nz>
2022-11-11 10:17:07 +1300

    first commit

---
+++ src/main/java/FrostBytePCs/App.java
@@ -0,0 +1,13 @@
+package FrostBytePCs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
